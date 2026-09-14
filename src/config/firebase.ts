f6f4b5ad7d1588dc7fd3e9991ad16e2c5960d2c7
MadeInRodri <rivasalexis1820@gmail.com>
2026-09-14 11:09:58 -0600

    Agregando configuracion inicial de firestore

---
+++ src/config/firebase.ts
@@ -0,0 +1,18 @@
+// src/config/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exportar la instancia de la base de datos para consumirla en la app
+export const db = getFirestore(app);
